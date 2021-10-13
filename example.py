248ee27b5d7f1c6a86a8d45437fa72b7303dda12
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-10-13 22:18:14 +0530

    add comment

--- example.py
+++ example.py
@@ -12,6 +12,7 @@ class ObjectA:
         self.x = x
         self.y = y
 
+    # Only wrapped method calls are tracked.
     @chainer.lazy
     def foo(self, x):
         self.x = x
@@ -23,17 +24,22 @@ class ObjectA:
         return y + 2
 
 def return_handle():
+    # Enable tracking of calls
     with chainer:
         a = ObjectA("Random Name", 25).foo(42)
         a = a.bar(25).bar(26)
     
     return a
 
+# `execute` actually executes the method chain
+# and clears the tracker
 assert 28 == return_handle().execute()
 
 with chainer:
     ObjectA("", 25).foo("").bar(26).bar(26)
-    assert chainer.execute() == 28
+# Can directly call the `execute` on the Chain object
+assert chainer.execute() == 28
 
+# Eager evaluation as usual.
 chainer.lazy_eval = False
 assert 28 == ObjectA("", 25).foo("").bar(27).bar(26)
